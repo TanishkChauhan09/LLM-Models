c0a67c81970a28f9bbe2e9f1eb7dc59a654d8b37
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-08-22 08:30:43 +0530

    website creation code using command in terminal

--- Day4/index.js
+++ Day4/index.js
@@ -4,6 +4,7 @@ import readlineSync from "readline-sync";
 import { exec } from "child_process";
 import { promisify } from "util";
 import os from "os";
+import fs from "fs/promises";
 
 
 const platform = os.platform();
@@ -53,8 +54,36 @@ const executecommandDeclaration ={
     }
 }
 
+
+
+
+async function writeFileContent({ path, content }) {
+  try {
+    await fs.writeFile(path, content, "utf-8");
+    return `Successfully wrote content to ${path}`;
+  } catch (error) {
+    return `Error writing file: ${error}`;
+  }
+}
+
+const writeFileContentDeclaration = {
+  name: "writeFileContent",
+  description: "Writes given content into a file at the specified path",
+  parameters: {
+    type: "object",
+    properties: {
+      path: { type: "string", description: "Path of the file" },
+      content: { type: "string", description: "Code/text to write inside the file" }
+    },
+    required: ["path", "content"]
+  }
+};
+
+
+
 const availabletools = {
     executecommand:executecommand,
+    writeFileContent: writeFileContent
 }
 
 
@@ -75,6 +104,20 @@ const availabletools = {
              
             systemInstruction:`You are an website developer expert in making the very very attractive and responsive websites, you hve to create the frontend of the website by analysing the user's inpur ,
             You also have the access of the tools that can execute or run the terminal commands, you can use these tools to create the files or the folders,
+
+            also keep in mind that the website should be as attaractive as possible, so use the best practice of HTML,CSS and Javascript to create the website,
+            and all the things should be systematic and well organised in the folder structure as well as in website , website should 
+            have the best standard and attarctive with a good changeable background design ,and a good responsive design,
+
+            Also,website should also fit whether for the windows or mac or desktop or mobile phone,
+            and all the features shuld not be very large going out from from the screen, or oversized, it should be very responsive 
+            and size of features should be according to the fit for the requirements of the website that you are creating for the user,
+
+            if someone wants some websites like course selling in which you have to show differnt things ake sure that all shouls be aligned and in a proper structure for example all the available
+            courses should be in a structured format and all courses should have same things but oviously diffent prices and offers also at their bottom tip there should be some little bit of offer showing strip like ,
+            so in such things you please add some more additional required features from your own side to make the website more attractive and responsive also header and footer part should also looks good and with  light,calmful,attractive background and features like contact us ,about us, home,services,ets all should have a beautiful colour
+
+            you can also have the right to add some images if needed like for example you in course selling website you can add the course images and all the things in website like header body(main),footern should be of differnt background color to make the differntiate 
             
             Current user operating system is ${platform}, so you can use the terminal commands that are supporteed by the ${platform} operating system,
 
@@ -94,6 +137,13 @@ const availabletools = {
              6:then run the command to open the index.html file in the browser, so that the user can see the output of the website that you have created for him/her,
 
             You have to provide the terminal commands one by one to the user, so that the user can execute the commands in his/her terminal
+
+            // You have access to two tools:
+            1. executecommand → run terminal commands (mkdir, touch, etc.).
+            2. writeFileContent → write actual HTML, CSS, or JavaScript code into files.
+            Always use writeFileContent to add code instead of echo.
+
+            
            
             Remember you don't need to use echo commands during this website creation  process, you just need to use the terminal commands that are supported by yhe ${platform} operating system,
             also use like,
@@ -111,11 +161,11 @@ const availabletools = {
             }
             // ..... rest of the valid Javascript code for the calculator
             updateDisplay();
-            @ | Set-Content-Path "script.js"
+            '@ | Set-Content-Path "script.js"
              
             `,
             tools:[{
-                functionDeclarations :[executecommandDeclaration]
+                functionDeclarations :[executecommandDeclaration,writeFileContentDeclaration]
             }],
         }
      });
